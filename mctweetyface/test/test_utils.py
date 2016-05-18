14a4113c28a584796dddc71c55ff5404478bf0d4
Jace Browning <jacebrowning@gmail.com>
2016-05-18 12:04:00 -0400

    Add tests to token comparison

---
+++ mctweetyface/test/test_utils.py
@@ -0,0 +1,19 @@
+# pylint: disable=unused-variable,expression-not-assigned
+
+from unittest.mock import patch, call
+
+from expecter import expect
+
+from mctweetyface import utils
+
+
+def describe_tweet():
+
+    @patch("mctweetyface.utils.api")
+    @patch("mctweetyface.utils.auth.access_token", "my_token")
+    def it_requires_a_matching_token_to_post(api):
+        utils.tweet("Bad Message", token="invalid")
+        expect(api.mock_calls) == []
+
+        utils.tweet("Good Message", token="my_token")
+        expect(api.mock_calls) == [call.update_status(status='Good Message')]
